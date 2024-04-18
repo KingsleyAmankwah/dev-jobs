7ec79f5150b99be755d23962345d6ec7fd14d5db
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-18 11:38:55 +0000

    feat: Activate dark theme on job card component

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppService } from '../services/app.service';
 
 @Component({
   selector: 'app-job-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-card.component.html',
-  styleUrl: './job-card.component.css'
+  styleUrl: './job-card.component.css',
 })
 export class JobCardComponent {
+  public isDarkTheme = false;
 
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.appService.isDarkTheme.subscribe((darkTheme) => {
+      this.isDarkTheme = darkTheme;
+    });
+  }
 }
