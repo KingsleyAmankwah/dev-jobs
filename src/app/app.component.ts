933773898765eb1a80130a61357474ea1934a40b
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-18 11:38:26 +0000

    feat: Implement the darkTheme functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AppService } from './services/app.service';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dev-jobs';
-
   public isActive = false;
+  private isDarkTheme = false;
+
+  constructor(private appService: AppService, private renderer: Renderer2) {}
 
   public toggleButton() {
     this.isActive = !this.isActive;
+    this.appService.toggleTheme();
+  }
+
+  private applyTheme() {
+    if (this.isDarkTheme) {
+      this.renderer.setStyle(document.body, 'backgroundColor', '#121721');
+    } else {
+      this.renderer.setStyle(document.body, 'backgroundColor', '#f4f6f8');
+    }
+  }
+
+  ngOnInit() {
+    this.appService.isDarkTheme.subscribe((darkTheme) => {
+      this.isDarkTheme = darkTheme;
+      this.applyTheme();
+    });
   }
 }
