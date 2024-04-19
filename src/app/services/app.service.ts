59db5a02217e3a4c044b286bc575a7ebb007482a
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-04-19 16:25:21 +0000

    feat: Add methods to handle jobs functionality

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,14 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { jobStructure } from '../interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppService {
-  constructor() {}
-
+  private jobsUrl = '/assets/data.json';
   private isDarkThemeSubject = new BehaviorSubject<boolean>(false);
+
+  constructor(private http: HttpClient) {}
   isDarkTheme = this.isDarkThemeSubject.asObservable();
+
+  public getJobsUrl(): Observable<jobStructure[]> {
+    return this.http.get<jobStructure[]>(this.jobsUrl);
+  }
+
+  public getJobById(id: number): Observable<jobStructure | undefined> {
+    return this.getJobsUrl().pipe(
+      map((jobs) => jobs.find((job) => job.id === id))
+    );
+  }
 
   public toggleTheme() {
     const currentTheme = this.isDarkThemeSubject.value;
